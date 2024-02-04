32dafe7e7b84214fa673618cc1596ed70869a490
D.A.Pelasgus <jim@tsimouris.com>
2024-02-04 18:47:27 +0200

    Create setup_nix.py

---
+++ setup_nix.py
@@ -0,0 +1,24 @@
+# setup_nix.py
+# author: D.A.Pelasgus
+
+import os
+import subprocess
+from styles import style_text
+
+def install_nix():
+    print(style_text("Checking if Nix is installed..."))
+
+    # Check if Nix is already installed
+    if shutil.which('nix'):
+        print(style_text("Nix is already installed."))
+    else:
+        print(style_text("Nix not found. Installing Nix..."))
+
+        # Perform a multi-user installation using the official script with daemon flag
+        subprocess.run(['sh', '-c', "curl -L https://nixos.org/nix/install | sh -s -- --daemon"], check=True, stdout=subprocess.PIPE)
+
+def main():
+    install_nix()
+
+if __name__ == "__main__":
+    main()
